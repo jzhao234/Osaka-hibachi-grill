b0884c731cced0b9c3065e10ca18873f7a55a08a
jzhao234 <jzhao1155@gmail.com>
2026-02-18 21:01:04 -0500

    worked on main slogan card

---
+++ src/components/Home/MainSloganCard.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+
+export default function MainSloganCard() {
+    return(
+        <div className="rounded-2xl">
+            <h1 className="text-3xl luckiest"> Made fresh, served fast: </h1>
+            <h1 className="text-3xl luckiest pl-10 pt-5"> Hibachi, Sushi, and Boba. </h1>
+        </div>
+    )
+}
